f5bf357b351dabb4847b7ca41a189df3a248fdbd
Saharsh Shukla <saharsh.shukla2018@vitstudent.ac.in>
2021-06-14 21:39:56 +0530

    10 - Challenge

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
 // We start by Loading in the first module
 // This module is the file system module
-
-const fs = require('fs')
+// fs is the code for FileSystem we store the module in a variable
+// const fs = require('fs')
 // We use the WriteFileSync command to save a file called notes.txt
-fs.writeFileSync('notes.txt', 'This file was created with NODE.JS')
+// fs.writeFileSync('notes.txt', 'This file was created with NODE.JS')
+
+// No. 9 Challenge > Append a message to notes.txt
+/*
+1. Use appendFileSync to append the file.
+2. Comment out the current code
+ */
+
+// fs.appendFileSync('notes.txt', '\nThis file has been appended')
+
+// Challenge completed successfully - dated: 14th June, 21:26
+
+// We load in a JAVA script file
+/*const add = require('./utils')
+console.log(
+    'App.js'
+)
+
+console.log(add(2,3))*/
+
+/*
+* Challenge 10 > Define and use a function in a new file
+* 1. Create a file called Notes.js
+* 2. Create a function called get notes that returns, "Your notes"
+* 3. Export the function
+* 4. Load this in app.js
+* */
+
+/*const notes = require('./notes')
+
+console.log(notes())*/
+
+// Challenge Completed successfully - dated: 14th June, 21:40
+
+
+
+
+
+
+
+
---
+++ notes.js
@@ -0,0 +1,5 @@
+const getNotes = function () {
+    return "Your Notes are: "
+}
+
+module.exports = getNotes
