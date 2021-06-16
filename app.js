8d546c5b13bde1f8fed5195b5f9a81c32e002be5
Saharsh Shukla <saharsh.shukla2018@vitstudent.ac.in>
2021-06-16 11:34:02 +0530

    Lecture 14

--- app.js
+++ app.js
@@ -1,82 +1,59 @@
-// We start by Loading in the first module
-// This module is the file system module
-// fs is the code for FileSystem we store the module in a variable
-// const fs = require('fs')
-// We use the WriteFileSync command to save a file called notes.txt
-// fs.writeFileSync('notes.txt', 'This file was created with NODE.JS')
-
-// No. 9 Challenge > Append a message to notes.txt
-/*
-1. Use appendFileSync to append the file.
-2. Comment out the current code
- */
-
-// fs.appendFileSync('notes.txt', '\nThis file has been appended')
-
-// Challenge completed successfully - dated: 14th June, 21:26
-
-// We load in a JAVA script file
-/*const add = require('./utils')
-console.log(
-    'App.js'
-)
-
-console.log(add(2,3))*/
-
-/*
-* Challenge 10 > Define and use a function in a new file
-* 1. Create a file called Notes.js
-* 2. Create a function called get notes that returns, "Your notes"
-* 3. Export the function
-* 4. Load this in app.js
-* */
-
-/*const notes = require('./notes')
-
-console.log(notes())*/
-
-// Challenge Completed successfully - dated: 14th June, 21:40
-/*
-const chalk = require('chalk')
-console.log("App.js")
-const validator = require('validator')
-
-console.log(validator.isEmail('Example.com'))
-console.log(validator.isURL('https://www.github.com'))
-
-console.log(chalk.bold.green.inverse("Successfully Downloaded Chalk2.4.1"))
-
-*/
 const chalk = require('chalk')
 const yargs = require('yargs')
 const notes = require('./notes')
-//Commands
-// Add
+
 
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: "The title of the note",
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: "Body of the note",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+// Remove
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove notes',
     handler: function () {
-        console.log('Adding a new note')
+        console.log('Removing')
     }
 })
 
+// Read
 
-let getCommand;
-getCommand = function () {
-    return process.argv[2]
-};
-
-let checkCondition;
-checkCondition = function () {
-    if (getCommand() === "add") {
-        notes.appendNotes()
-    } else {
-        console.log(chalk.red.bold("Exit with Status -1"))
+yargs.command({
+    command: 'read',
+    describe: 'Gets all Notes',
+    handler: function () {
+        console.log('Reading')
     }
-};
-/*console.log(getCommand())
-checkCondition()*/
+})
 
-console.log(yargs.argv)
-// console.log(process.argv)
+// List
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists all notes',
+    handler: function () {
+        console.log('Listing')
+    }
+})
+
+// ************************** //
+
+yargs.parse()
+
